fc6b27547adbef8831dd2fa1360e95a1c289c154
wmadwand <wmadwand@gmail.com>
2018-09-28 15:25:45 +0300

    Extra stuff

--- MyStudy.cs
+++ MyStudy.cs
@@ -19,6 +19,7 @@ namespace Proxy01
     public interface IBook
     {
         Page GetPage(int number);
+        int GetPagesCount();
     }
 
     public class BookStore : IBook
@@ -34,6 +35,11 @@ namespace Proxy01
         {
             return db.pages.FirstOrDefault(p => p.number == number);
         }
+
+        public int GetPagesCount()
+        {
+            return db.pages.Count();
+        }
     }
 
     public class BookStoreProxy : IBook
@@ -41,10 +47,15 @@ namespace Proxy01
         private List<Page> _pages; // our local cache list
         private BookStore _bookStore; // remote storage
 
-        public BookStoreProxy()
+        private int _pagesCountOrigin; // count of pages are expected to be containted into the book
+                                       // so as to we could prepare free space onto the shelf in advance
+
+        public BookStoreProxy(int pagesCountOrigin)
         {
             _pages = new List<Page>();
             _bookStore = new BookStore();
+
+            _pagesCountOrigin = pagesCountOrigin;
         }
 
         public Page GetPage(int number)
@@ -60,13 +71,18 @@ namespace Proxy01
 
             return page;
         }
+
+        public int GetPagesCount()
+        {
+            return _pagesCountOrigin;
+        }
     }
 
     public class Main
     {
         public void Start()
         {
-            IBook book = new BookStoreProxy();
+            IBook book = new BookStoreProxy(100);
 
             Page page01 = book.GetPage(1);
             Console.WriteLine(page01.text);
